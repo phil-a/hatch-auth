b05e6639ccdf9135a91a4bc84ecce183561f852c
Philip Alekseev <phil.alekseev@gmail.com>
2016-04-17 21:17:27 -0400

    mock props to TaskItem

--- src/components/dashboard/TaskItem.js
+++ src/components/dashboard/TaskItem.js
@@ -3,15 +3,22 @@ var {
   View,
   Text,
   Image,
+  TouchableOpacity,
   StyleSheet
 } = React;
 
 module.exports = React.createClass({
+  _onPressButton(e){
+    alert(e.target);
+  },
   render: function() {
     return (
-      <View style={styles.card}>
-        <Text>Task - 1</Text>
-      </View>
+      <TouchableOpacity onPress={this._onPressButton}>
+        <Image source={{uri: this.props.imageURL}} resizeMode='cover' style={styles.card}>
+          <View style={styles.header}><Text style={styles.headerText}>{this.props.text}</Text></View>
+          <View style={styles.body}><Text style={styles.bodyText}>{this.props.desc}</Text></View>
+        </Image>
+      </TouchableOpacity>
     );
   }
 });
@@ -19,14 +26,35 @@ module.exports = React.createClass({
 var styles = StyleSheet.create({
   card: {
     borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
     backgroundColor: '#fff',
     borderColor: 'rgba(0,0,0,0.1)',
     margin: 5,
     height: 150,
-    padding: 15,
     shadowColor: '#ccc',
     shadowOffset: { width: 2, height: 2, },
     shadowOpacity: 0.5,
     shadowRadius: 3,
+  },
+  header: {
+    alignSelf: 'stretch',
+    backgroundColor: 'rgba(0,0,0,0.35)',
+    padding: 10,
+  },
+  body: {
+    alignSelf: 'stretch',
+    backgroundColor: 'rgba(0,0,0,0.35)',
+    padding: 2,
+  },
+  headerText: {
+    alignSelf: 'center',
+    fontSize: 20,
+    color: 'whitesmoke',
+  },
+  bodyText: {
+    justifyContent: 'flex-end',
+    fontSize: 15,
+    color: 'linen',
   }
 });
--- src/components/dashboard/TasksContainer.js
+++ src/components/dashboard/TasksContainer.js
@@ -13,10 +13,10 @@ module.exports = React.createClass({
   render: function() {
     return (
       <ScrollView tabLabel="ios-checkmark" style={styles.tabView}>
-        <TaskItem />
-        <TaskItem />
-        <TaskItem />
-        <TaskItem />
+        <TaskItem text="Kitten" desc="This is a cute kitten." imageURL="https://lh6.ggpht.com/sw_iT7GZASdAYeiecsZEHJE-EgDhdK2rCWUzZOJS0OFiGpoi9qn8iMH2nuXHgWg2PA=h900"/>
+        <TaskItem text="Puppy" desc="This is a lazy dog." imageURL="http://indiabright.com/wp-content/uploads/2015/12/cute-puppy.jpg"/>
+        <TaskItem text="Piglet" desc="This is a happy pig." imageURL="http://s2.favim.com/orig/34/carolina-wang-cute-little-mini-pig-Favim.com-274154.jpg"/>
+        <TaskItem text="Fennec" desc="This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox. This is a sly fox." imageURL="http://cdn.attackofthecute.com/October-05-2012-02-00-34-nsbdnbfndbfd.jpeg"/>
       </ScrollView>
     );
   }
